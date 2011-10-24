f107fcd16b8b9e07b1ff0dc285fd9392abaabdf5
cristian <cristian.ivanoff@gmail.com>
2011-10-24 21:09:51 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tickets::Application.config.secret_token = '7b05e2c97459cc8215215611f496bfdf4c241c26f2bc5a967697ead5b581cc08c9908a154629df021e490635433e9d94006edec018be72faa4e7bff1f3641d22'
---
+++ db/migrate/20111012214441_add_phone_to_tickets.rb
@@ -0,0 +1,5 @@
+class AddPhoneToTickets < ActiveRecord::Migration
+  def change
+    add_column :tickets, :phone, :string
+  end
+end
